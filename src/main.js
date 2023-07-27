64b69d346c11185a5790697e292753ff1076d036
markzv <134307722+markvayson@users.noreply.github.com>
2023-07-27 11:50:20 +0400

    Change draw board color

--- src/main.js
+++ src/main.js
@@ -277,7 +277,9 @@ const displayController = (() => {
       }
     }
     if (isEmpty) {
-      boxElements.forEach((box) => box.classList.add("pointer-events-none"));
+      boxElements.forEach((box) => {
+        box.classList.remove("bg-red-500","bg-purple-500")
+        box.classList.add("pointer-events-none","bg-slate-600")});
       turnTexts[0].textContent = "";
       turnTexts[1].textContent = "";
       return (turnTexts[2].textContent = "Draw!");
